3cc01a1ae4c1fb908f369464f77a146ff9fc2d3e
221250032 <1580187324@qq.com>
2024-04-25 10:22:21 +0800

    fix:修改了coupon.ts

---
+++ src/api/coupon.ts
@@ -0,0 +1,44 @@
+import {axios} from '../utils/request'
+import {COUPON_MODULE} from './_prefix'
+
+type CouponGroupInfo = {
+    amount: number,
+    storeId: number,
+    userId: number,
+    type: string,
+    Full: number,
+    Reduction: number,
+}
+
+type CouponInfo = {
+    userId: number,
+    storeId: number,
+    couponGroupId: number,
+    type : string;
+    Full: number,
+    Reduction: number,
+}
+
+export const createGroup = (couponGroupInfo: CouponGroupInfo) => {
+    return axios.post(`${COUPON_MODULE}/`, couponGroupInfo,
+        {headers: {'Content-Type': 'application/json'}})
+        .then(res => {
+            return res
+        })
+}
+
+export const getAllGroup = () => {
+    return axios.get(`${COUPON_MODULE}/`)
+        .then(res => {
+            return res
+        })
+
+}
+
+export const receiveCoupon = (couponInfo:CouponInfo) => {
+    return axios.post(`${COUPON_MODULE}/receive`, couponInfo,
+        {headers: {'Content-Type': 'application/json'}})
+        .then(res => {
+            return res
+        })
+}
